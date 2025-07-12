9596928fa8871d5739eb01f88a046d63426e109c
EDSONRD84 <edsonrd@gmail.com>
2025-07-12 02:19:14 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,36 +1,10 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/api/produtos', (req, res) => {
-  const file = path.join(__dirname, 'data', 'produtos.json');
-  if (fs.existsSync(file)) {
-    const json = fs.readFileSync(file, 'utf-8');
-    res.json(JSON.parse(json));
-  } else {
-    res.status(404).json({ erro: 'produtos.json não encontrado' });
-  }
-});
-
-app.get('/api/precos', (req, res) => {
-  const file = path.join(__dirname, 'data', 'precos.json');
-  if (fs.existsSync(file)) {
-    const json = fs.readFileSync(file, 'utf-8');
-    res.json(JSON.parse(json));
-  } else {
-    res.status(404).json({ erro: 'precos.json não encontrado' });
-  }
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 app.listen(PORT, () => {
-  console.log("Servidor rodando em http://localhost:" + PORT);
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
